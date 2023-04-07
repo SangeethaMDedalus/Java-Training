a79c9451c8245bbf0c75d9db6596e4560c2a0d1a
SangeethaMDedalus <125359001+SangeethaMDedalus@users.noreply.github.com>
2023-04-07 12:40:42 +0530

    Add files via upload

---
+++ GenericsUBLB.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class GenericsUBLB
+{
+	//Integer is Lowerbound
+	public static void addNumToList(List<? super Integer> list)
+	{
+		for(int i=0;i<10;i++)
+		{
+			list.add(i);
+		}
+		System.out.println(list);
+	}
+	
+	//Number is the Upperbound
+	public static void sumallNum(List<? extends Number> list)
+	{
+		double d =0;
+		for(Number n :list)
+		{
+			d=d+n.doubleValue();
+		}
+		System.out.println("small Num"+d);
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		List<Object> l1 = new ArrayList<Object>();
+		addNumToList(l1);
+		List<Number> l2 = new ArrayList<Number>();
+		addNumToList(l2);
+
+		List<Integer> a = Arrays.asList(1,2,3);
+		sumallNum(a);
+
+		List<Object> sample = Arrays.asList(1,2,3,1.1,"sample string");
+		System.out.println("hetero"+sample);
+		//sumallNum(a);
+		
+		List<Double> b = Arrays.asList(1.1,2.1,3.1);
+		sumallNum(b);
+
+	}
+}
